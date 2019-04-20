1aeb52b8f9ba27ef0ea78e5d1889d755b51422c6
Savannah C <sgcorrell@gmail.com>
2019-04-20 18:40:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,12 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    list = Nokogiri::HTML
   end
 
   def self.scrape_profile_page(profile_url)
