f8b0c447fb46ea2cb4ac84ad54a23500d56bf35a
Savannah C <sgcorrell@gmail.com>
2019-04-20 18:41:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,10 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     list = Nokogiri::HTML
+    
+    students = []
+    
+    
   end
 
   def self.scrape_profile_page(profile_url)
